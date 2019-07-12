53735d99755c12d003fb3521885228647bc17cb5
Bill Chirico <billchirico@gmail.com>
2019-07-12 13:07:32 -0400

    Add service provider Add hosts

--- src/Volvox.Enigma.Console/Program.cs
+++ src/Volvox.Enigma.Console/Program.cs
@@ -1,13 +1,35 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Volvox.Enigma.Domain.User;
 
 namespace Volvox.Enigma.Console
 {
-    class Program
+    internal class Program
     {
         public static async Task Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            var serviceProvider = ConfigureServiceCollection(new ServiceCollection());
+        }
+
+        private static ServiceProvider ConfigureServiceCollection(ServiceCollection serviceCollection)
+        {
+            return serviceCollection
+                .Configure<List<Host>>(GetSettingsFile("hosts.json", "Hosts"))
+                .BuildServiceProvider();
+        }
+
+        private static IConfigurationSection GetSettingsFile(string file, string section)
+        {
+            var builder = new ConfigurationBuilder();
+
+            builder
+                .AddJsonFile(file, false, true);
+
+            var configuration = builder.Build();
+
+            return configuration.GetSection(section);
         }
     }
 }
---
+++ src/Volvox.Enigma.Domain/User/Host.cs
@@ -0,0 +1,9 @@
+namespace Volvox.Enigma.Domain.User
+{
+    public class Host
+    {
+        public ulong DiscordId { get; set; }
+
+        public string TwitchUsername { get; set; }
+    }
+}
