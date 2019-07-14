c19df42889c3a6e798a293e8b6dcf04040a8d319
Bill Chirico <billchirico@gmail.com>
2019-07-14 12:51:59 -0400

    Remove Service project dependency on DI

--- src/Volvox.Enigma.Service/StreamAnnouncer/StreamAnnouncer.cs
+++ src/Volvox.Enigma.Service/StreamAnnouncer/StreamAnnouncer.cs
@@ -1,15 +1,6 @@
-using Microsoft.Extensions.Options;
-using Volvox.Enigma.Domain.User;
-
 namespace Volvox.Enigma.Service.StreamAnnouncer
 {
     public class StreamAnnouncer : IStreamAnnouncer
     {
-        private readonly Hosts _hosts;
-
-        public StreamAnnouncer(IOptions<Hosts> hosts)
-        {
-            _hosts = hosts.Value;
-        }
     }
 }
