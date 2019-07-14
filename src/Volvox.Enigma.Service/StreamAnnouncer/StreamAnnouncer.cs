52f61d72ee26ef3c9d7d57c15be1b76606f606d1
Bill Chirico <billchirico@gmail.com>
2019-07-14 12:53:48 -0400

    Add initial Announce method

--- src/Volvox.Enigma.Service/StreamAnnouncer/StreamAnnouncer.cs
+++ src/Volvox.Enigma.Service/StreamAnnouncer/StreamAnnouncer.cs
@@ -1,6 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Volvox.Enigma.Domain.User;
+
 namespace Volvox.Enigma.Service.StreamAnnouncer
 {
     public class StreamAnnouncer : IStreamAnnouncer
     {
+        private readonly DiscordSocketClient _discordClient;
+
+        public StreamAnnouncer(DiscordSocketClient discordClient)
+        {
+            _discordClient = discordClient;
+        }
+
+        public Task Announce(List<Host> hosts)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
